518d89874e3231d52e1383c4d45fb4468914766f
francesca <fradeluca85@gmail.com>
2021-11-17 20:32:49 +0100

    livelli difficoltà con bottoni

--- js/script.js
+++ js/script.js
@@ -11,24 +11,87 @@ const container = document.querySelector(".container");
 
 let items= '';
 
-for(let i = 1; i <= 81; i++){
-    items +=`
-    <div class="square hard">${i}</div>`    
-}
-for(let i = 1; i <= 100; i++){
-    items +=`
-    <div class="square easy">${i}</div>`    
-}
-for(let i = 1; i <= 49; i++){
-    items +=`
-    <div class="square crazy">${i}</div>`    
-}
-console.log(items);
-container.innerHTML = items
+// for(let i = 1; i <= 81; i++){
+//     items +=`
+//     <div class="square hard">${i}</div>`    
+    
+// }
+// for(let i = 1; i <= 100; i++){
+//     items +=`
+//     <div class="square easy">${i}</div>`    
+// }
+// for(let i = 1; i <= 49; i++){
+//     items +=`
+//     <div class="square crazy">${i}</div>`    
+// }
+// console.log(items);
+// container.innerHTML = items
 
 
-let divSquare = document.querySelector('.square')
-divSquare.addEventListener("click", function(){
-    this.classList.add('blue'); 
-});
-console.log(divSquare);
+// let divSquare = document.querySelector('.square')
+// divSquare.addEventListener("click", function(){
+//     this.classList.add('blue'); 
+// });
+// console.log(divSquare);
+
+function easy(){
+    const levEasy = function () {
+        const node = document.createElement('div');
+        node.className = 'square easy';
+    
+        return node;
+    }
+    
+    for(let i = 0; i < 100; i++){
+    
+        const clicked = levEasy();
+        container.appendChild(clicked);
+    
+        clicked.addEventListener('click', function() {
+            console.log(this);
+            this.classList.add('blue');
+        });
+    }
+
+}
+
+function hard (){
+    const levHard = function () {
+        const node = document.createElement('div');
+        node.className = 'square hard';
+    
+        return node;
+    }
+    
+    for(let i = 0; i < 81; i++){
+    
+        const clicked = levHard();
+        container.appendChild(clicked);
+    
+        clicked.addEventListener('click', function() {
+            console.log(this);
+            this.classList.add('blue');
+        });
+    
+    }
+}
+function crazy (){
+    const levCrazy = function () {
+        const node = document.createElement('div');
+        node.className = 'square crazy';
+    
+        return node;
+    }
+    
+    for(let i = 0; i < 49; i++){
+    
+        const clicked = levCrazy();
+        container.appendChild(clicked);
+    
+        clicked.addEventListener('click', function() {
+            console.log(this);
+            this.classList.add('blue');
+        });
+    
+    }
+}
